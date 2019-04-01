fb809e2b8dc3533861d52725f0ae5ed4891305a8
Yaroslav Polyakov <yaroslaff@gmail.com>
2019-04-02 05:11:30 +0700

    initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hashget-kernel_org',
+    version='0.3',
+    description='',
+    long_description='',
+    author='Yaroslav Polyakov',
+    author_email='yaroslaff@gmail.com',
+
+    license='Apache Software License',
+
+    packages=['hashget.heuristics.kernel_org'],
+    zip_safe=False,
+)
+
