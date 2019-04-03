81823bf78616fec014ce2953d78b387ee9fc6d89
Yaroslav Polyakov <yaroslaff@gmail.com>
2019-04-03 07:50:26 +0700

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+This is [https://kernel.org/](kernel.org) heuristic plugin for [Hashget](https://gitlab.com/yaroslaff/hashget/) deduplication tool.
+
+Also, it could be used as an example to develop other heuristics plugins for hashget.
+
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='hashget-kernel_org',
-    version='0.3',
-    description='',
-    long_description='',
+    version='0.5',
+    description='kernel.org (linux kernel sources) plugin for hashget',
+    long_description='kernel.org (linux kernel sources) plugin for hashget',
     author='Yaroslav Polyakov',
     author_email='yaroslaff@gmail.com',
 
