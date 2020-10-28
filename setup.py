144587e5ec1e556cd9b1ba89f6736771ac7c3d23
Yaroslav Polyakov <yaroslaff@gmail.com>
2020-10-29 02:30:04 +0700

    added link to repo

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='hashget-kernel_org',
     version='0.7',
+    url='https://github.com/yaroslaff/hashget',
+
     description='kernel.org (linux kernel sources) plugin for hashget',
     long_description='kernel.org (linux kernel sources) plugin for hashget',
     author='Yaroslav Polyakov',
